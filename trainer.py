67f4fa22115d1620f73c6aad74d4295557eb1c79
MaryemBenslimene <93080200+MaryemBenslimene@users.noreply.github.com>
2023-04-09 21:18:27 +0100

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -16,7 +16,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import sys
 from torch.utils.data import DataLoader
+from itertools import product
 
 import submitit
 from datetime import datetime
@@ -236,6 +238,14 @@ def train_tsp(args):
     from tasks import tsp
     from tasks.tsp import TSPDataset
 
+
+    #wandb.init(project="PFA-routing" + str(config['generations']), config=config, mode='online')       
+    #wandb.run.name = wandb.run.name = "Generations" + str(config['generations'] )+'mutation_rate' + str(config['mutation_rate']) + 'population_size' + str(config['population_size']) +'elite_pct'+ str(config['elite_pct'])
+    #wandb.config.update(config)
+
+    wandb.init(project = 'PFA ERL' , mode = 'online')
+    wandb.run.name = 'test PFA with 6 nodes'
+
     STATIC_SIZE = 2 # (x, y)
     DYNAMIC_SIZE = 1 # dummy for compatibility
 
@@ -246,7 +256,9 @@ def train_tsp(args):
 
     critic = StateCritic(STATIC_SIZE, DYNAMIC_SIZE, args.hidden_size).to(device)
 
-    GAOptim = GA.GAOptimizer(generations=20, mutation_rate=0.1, population_size=10, elite_pct=0.1)
+    #GAOptim = GA.GAOptimizer(generations=config['generations'], mutation_rate=config['mutation_rate'], population_size=config['population_size'], elite_pct=config['population_size'])
+    
+    GAOptim = GA.GAOptimizer(generations=100, mutation_rate=0.1, population_size=20, elite_pct=0.1)
     GAOptim.population = GAOptim.create_population(STATIC_SIZE, DYNAMIC_SIZE, update_fn, args)
 
     kwargs = vars(args)
@@ -259,7 +271,7 @@ def train_tsp(args):
         print("started")
         print("gen", generation)
         fitness = []    
-        #wandb.log({'Generation': generation})
+        wandb.log({'Generation': generation})
         for individual in GAOptim.population :
             print('indiv')
             if args.checkpoint:
@@ -270,27 +282,27 @@ def train_tsp(args):
                 critic.load_state_dict(torch.load(path, device))
 
             if not args.test:
-                print("train")
                 train(individual, critic, **kwargs)
 
             test_data = TSPDataset(args.num_nodes, args.train_size)
-            print("test")
             test_dir = 'test'
             test_loader = DataLoader(test_data, args.batch_size, False, num_workers=0)
             reward = validate(test_loader, individual, tsp.reward, tsp.render, test_dir, num_plot=5)
 
             fitness.append(reward)
-            print("reward = ", reward)
-            print("fitness = ", fitness)
-            #wandb.log({'Validation reward': out})
+            wandb.log({'Validation reward': reward})
+
+        mean = sum(fitness) / GAOptim.population_size
+        wandb.log({'Mean fitness over generations': mean})
         parents = GAOptim.parents_selection(fitness)
         offsprings = GAOptim.crossover(parents, STATIC_SIZE, DYNAMIC_SIZE, update_fn, args)
         GAOptim.population = parents + offsprings
         GAOptim.mutation()
     best_model_index = fitness.index(min(fitness))
     actor = GAOptim.population[best_model_index]
-    #wandb.log({'Average tour length': fitness['best_model_index']})
     print('Average tour length: ', fitness[best_model_index])
+
+    wandb.finish()
 
 
 
@@ -364,29 +376,65 @@ def train_vrp(args):
 
     print('Average tour length: ', out)
 
+'''''
+PARAM_GRID = list(product(
+    [10,20],  # generations
+    [0.1,0.2,0.3],  # mutation_rate
+    [10],  # population_size
+    [0.1],  # elite_pct
+))
+'''''
+
+
+
+#parser.add_argument('--seed', default=12345, type=int)
+
+
+#parser.add_argument('--generations', type=int, default=10)
+#parser.add_argument('--mutation_rate', type=float, default=0.1)
+#parser.add_argument('--population_size', type=int, default=5)
+#parser.add_argument('--elite_pct', type=float, default=0.2)
 
 if __name__ == '__main__':
 
-    #wandb.init(project="PFA ERL", mode='online')          
-    #wandb.run.name = "test PFA"
 
     parser = argparse.ArgumentParser(description='Combinatorial Optimization')
-    #parser.add_argument('--seed', default=12345, type=int)
     parser.add_argument('--checkpoint', default=None)
     parser.add_argument('--test', action='store_true', default=False)
     parser.add_argument('--task', default='tsp')
-    parser.add_argument('--nodes', dest='num_nodes', default=20, type=int)
+    parser.add_argument('--nodes', dest='num_nodes', default=6, type=int)
     parser.add_argument('--actor_lr', default=5e-4, type=float)
     parser.add_argument('--critic_lr', default=5e-4, type=float)
-    parser.add_argument('--max_grad_norm', default=2., type=float)
+    parser.add_argument('--max_grad_norm', default=6., type=float)
     parser.add_argument('--batch_size', default=256, type=int)
     parser.add_argument('--hidden', dest='hidden_size', default=128, type=int)
     parser.add_argument('--dropout', default=0.1, type=float)
     parser.add_argument('--layers', dest='num_layers', default=1, type=int)
-    parser.add_argument('--train-size',default=1000000, type=int)
+    parser.add_argument('--train-size',default=10000, type=int) # na9ast zouz sfar
     parser.add_argument('--valid-size', default=1000, type=int)
 
+
     args = parser.parse_args()
+    '''''
+    hyper_list = []
+    for param_ix in range(len(PARAM_GRID)):
+
+        params = PARAM_GRID[param_ix]
+
+        g,m,p,e = params
+        if len(sys.argv) > 1:
+            g = args.generations
+            m = args.mutation_rate
+            p = args.population_size
+            e = args.elite_pct
+        config = {}
+        config['generations'] = g
+        config['mutation_rate'] = m
+        config['population_size']=p
+        config['elite_pct'] = e
+        if config not in hyper_list:
+            hyper_list += [config]
+    '''''
 
     #print('NOTE: SETTTING CHECKPOINT: ')
     #args.checkpoint = os.path.join('vrp', '10', '12_59_47.350165' + os.path.sep)
@@ -395,13 +443,13 @@ if __name__ == '__main__':
     d = datetime.today()
     exp_dir = (
             Path("./submitit-res")
-            / f"{d.strftime('%Y-%m-%d')}gen10_rand_eval"
+            / f"{d.strftime('%Y-%m-%d')}_20nodesrand_eval"
     )
     exp_dir.mkdir(parents=True, exist_ok=True)
     submitit_logdir = exp_dir / "submitit_logs"
     executor = submitit.AutoExecutor(folder=submitit_logdir)
     executor.update_parameters(
-        timeout_min=60 * 24,
+        timeout_min= 60 * 300,
         #slurm_partition="long",
         slurm_additional_parameters={"account": "def-bengioy"},
         gpus_per_node=1,
@@ -411,10 +459,16 @@ if __name__ == '__main__':
     )
 
     if args.task == 'tsp':
+        #job = executor.map_array(train_tsp,hyper_list)
         job = executor.submit(train_tsp,args)
     elif args.task == 'vrp':
         train_vrp(args)
     else:
         raise ValueError('Task <%s> not understood'%args.task)
     
-    print("# Submitted job")
+    print(f"# Submitted job")
+    #print(f"# Submitted job {len(hyper_list)}")
+
+    
+
+    
